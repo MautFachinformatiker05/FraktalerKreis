50a5f65f2023a61d6f7d953ee2551f82c320fb88
Filip Golanski <filip.golanski@googlemail.com>
2020-04-08 12:16:14 +0200

    added Methods to SerializableLine

--- FraktalerKreis/src/application/SerializableLine.java
+++ FraktalerKreis/src/application/SerializableLine.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import javafx.scene.shape.Line;
@@ -10,5 +12,28 @@ public class SerializableLine extends Line implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -5529625571836590418L;
+	
+	private void writeObject(ObjectOutputStream os) {
+	   
+	   try {
+	    os.defaultWriteObject();
+	    os.writeDouble(getStartX());
+	    os.writeDouble(getStartY());
+	    os.writeDouble(getEndX());
+	    os.writeDouble(getEndX());
+	    os.writeDouble(getStrokeWidth());
+	   } catch (Exception e) { e.printStackTrace(); }
+	  }
 
+	  private void readObject(ObjectInputStream is) {
+	    
+	   try {
+	    is.defaultReadObject();
+	    setStartX(is.readDouble());
+	    setStartY(is.readDouble());
+	    setEndX(is.readDouble());
+	    setEndY(is.readDouble());
+	    setStrokeWidth(is.readDouble());
+	   } catch (Exception e) { e.printStackTrace(); }
+	  }
 }
